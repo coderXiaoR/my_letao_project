0b3de8822fe27237937b93c7a6579a705365ad68
xiaoR <xiaorui19910323@126.com>
2017-11-09 19:25:45 +0800

    所有页面基本公共结构完成v1.0

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -11,4 +11,42 @@ $(function () {
       NProgress.done(500)
     }, 800);
   })
+
+  // 验证登录状态的接口
+  //非登录页都要验证
+  if (location.href.indexOf('login.html') < 0) {
+    //为非登录页
+    $.ajax({
+      url:'/employee/checkRootLogin',
+      success:function (data) {  
+        if (data.error === 400) {
+          //登陆失败
+          location.href = 'login.html';
+        }
+      }
+    })
+  }
+
+  //退出登录按钮
+  $('.btn_confirm').on('click',function () {  
+    $.ajax({
+      url:'/employee/employeeLogout',
+      success:function (data) {  
+        if (data.success) {
+          location.href = 'login.html'
+        }
+      }
+    })
+  })
+
+  //左上菜单按钮
+  $('.btn_menu').on('click',function () {
+    console.log(11111);  
+    $('#lt_aside').toggleClass('hidden_self');
+    $('#lt_main').toggleClass('full_width');
+  })
+  //点击切换二级菜单
+  $('.side_nav li:eq(1)>a').on('click',function () {  
+    $('.child').slideToggle();
+  })
 });
